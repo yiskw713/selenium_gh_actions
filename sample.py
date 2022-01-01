2dc82bafc6c511ed9f50bc00f55b5ee1d105aa14
Yuchi Ishikawa <i.yuchi713@gmail.com>
2022-01-01 12:47:18 +0900

    Code refactoring

--- sample.py
+++ sample.py
@@ -1,10 +1,17 @@
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
 
-options = Options()
-# ヘッドレスモード（Linux上で動かすとき必ずこのモードにしておく）
-options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
-driver.get('https://google.com')
-print(driver.current_url)
+
+def main() -> None:
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://google.com")
+
+    print(f"Current ULR: {driver.current_url}")
+
+
+if __name__ == "__main__":
+    main()
